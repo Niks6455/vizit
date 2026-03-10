104a4df6607d4f1b27668ff8c2058fb6f283cff3
Your Name <you@example.com>
2026-03-11 00:27:27 +0300

    feat(ABIT-#): поправил роуты для картинокэ

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: { unoptimized: true },
   ...(process.env.BASE_PATH && {
     basePath: process.env.BASE_PATH,
     assetPrefix: process.env.BASE_PATH,
